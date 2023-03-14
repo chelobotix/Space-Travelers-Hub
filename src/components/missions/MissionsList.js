f57375723870ef9af37429fd1559a570fc9c1781
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-03-14 16:37:37 -0400

    Add Mission List and Mission from API

--- src/components/missions/MissionsList.js
+++ src/components/missions/MissionsList.js
@@ -1,5 +1,47 @@
+import { v4 as uuidv4 } from 'uuid';
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, fetchMissions } from "../../redux/features/missions/missionsSlice";
+import Mission from "./Mission";
+import style from './MissionsList.module.css'
+
 const MissionsList = () => {
-  return <h1>Missions</h1>
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.missions)
+
+
+  useEffect(() => {
+    dispatch(fetchMissions())
+
+  }, [dispatch])
+
+  useEffect(() => {
+    console.log(state.missionsArray)
+
+
+  }, [state])
+
+  if (!state.missionsArray) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div>
+      <table className={style.table}>
+        <thead>
+          <tr>
+            <th className={style.border}>Mission</th>
+            <th className={style.border}>Description</th>
+            <th className={style.border}>Status</th>
+            <th className={style.border}></th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.missionsArray.map((mission) => <tr key={uuidv4()}><Mission mission={mission} /></tr>)}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default MissionsList;
---
+++ src/redux/features/missions/missionsSlice.js
@@ -0,0 +1,52 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  missionsArray: null,
+  isLoading: false,
+  isError: false,
+}
+
+export const fetchMissions = createAsyncThunk('get/missions', async () => {
+  const url = 'https://api.spacexdata.com/v3/missions'
+  const response = await fetch(url)
+  return response.json()
+})
+
+export const missionsSlice = createSlice({
+  name: 'Missions',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchMissions.pending, (state) => {
+      return {
+        ...state,
+        isLoading: true,
+      }
+    })
+
+    builder.addCase(fetchMissions.fulfilled, (state, action) => {
+      return {
+        ...state,
+        isLoading: false,
+        missionsArray: action.payload,
+      }
+    })
+
+    builder.addCase(fetchMissions.rejected, (state, action) => {
+      console.log('Error', action.error.message)
+      return {
+        ...state,
+        isError: true,
+        isLoading: false,
+      }
+    })
+  }
+})
+
+export const { increment } = missionsSlice.actions;
+
+export default missionsSlice.reducer;
